7f883ed0f77b0c4ae4968db0bca7351ede3a6f76
DESKTOP-67BT1F3\Panda <chrisrberg03@gmail.com>
2025-03-10 14:52:18 +0100

    test node server mongodb

---
+++ models/product.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "please enten a name"],
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        Timestamps: true,
+    }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
